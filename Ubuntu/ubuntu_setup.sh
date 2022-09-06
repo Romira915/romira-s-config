1aa27c8fc34a422466386a22dd1d649e9719b4c4
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-09-07 07:45:02 +0900

    add: wakeonlan

--- Ubuntu/ubuntu_setup.sh
+++ Ubuntu/ubuntu_setup.sh
@@ -9,7 +9,7 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install basic module
-sudo apt install -y build-essential procps curl file git lldb cmake
+sudo apt install -y build-essential procps curl file git lldb cmake wakeonlan
 
 # Setting zsh
 sudo apt install -y zsh
