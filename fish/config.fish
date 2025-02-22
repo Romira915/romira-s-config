8e172b93f4fad7e37d060f152fd0fab9e6c461d0
Romira915 <40430090+Romira915@users.noreply.github.com>
2025-02-22 21:24:13 +0900

    fix: add bw-agent

--- fish/config.fish
+++ fish/config.fish
@@ -37,5 +37,15 @@ end
 
 source ~/.config/mcfly/mcfly.fish
 
+function bw-add-ssh
+  set -l item_id "d8778a92-7c5f-481d-a046-fa59ecb65132"
+  set -l tmp_key (mktemp -p /tmp bw-ssh-key.XXXXXX)
+
+  bw get item $item_id | jq -r '.sshKey.privateKey' > $tmp_key
+  chmod 600 $tmp_key
+  ssh-add $tmp_key
+  rm -f $tmp_key
+end
+
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/yuudai.tanaka/.tmp/google-cloud-sdk/path.fish.inc' ]; . '/Users/yuudai.tanaka/.tmp/google-cloud-sdk/path.fish.inc'; end
