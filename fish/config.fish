d00574d00475be06adb472adf3301d7294a2c509
Romira915 <40430090+Romira915@users.noreply.github.com>
2026-02-23 21:14:25 +0900

    fix

--- fish/config.fish
+++ fish/config.fish
@@ -17,3 +17,7 @@ starship init fish | source
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "$HOME/.tmp/google-cloud-sdk/path.fish.inc" ]; . "$HOME/.tmp/google-cloud-sdk/path.fish.inc"; end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
